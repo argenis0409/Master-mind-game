49d0f8a7b77d7434f91b962b1793496cbe4ebcda
Argenis De Los Santos <argenis04_09@icloud.com>
2019-12-16 23:39:01 -0800

    modified the card,js file

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,128 +1,102 @@
 import React, { Component } from 'react'
-import Card from './Cards';
-import CardsView from './CardsView';
+import Card from './Card';
 
 export default class Game extends Component {
     constructor(props) {
-        super(props);
-        this.cardClicked = this.cardClicked.bind(this);
-        this.playAgain = this.playAgain.bind(this);
-        this.memoryCards = new MemoryCards();
-      }
-    
-      componentWillMount() {
-        this.startGame();
-      }
-    
-      startGame() {
-        this.memoryCards.generateCardSet();
-        this.setState({
-          turn : 1,
-          pairsFound : 0,
-          numClicksWithinTurn : 0,
-          firstId : undefined,
-          secondId : undefined
-        });
-      }
-    
-      getCardViews() {
-        let cardViews = [];
-        let onClick = this.cardClicked;
-        this.memoryCards.cards.forEach(c => {
-          let cardView = <CardView key={c.id} 
-              id={c.id} 
-              image={c.image}
-              imageUp = {c.imageUp}
-              matched = {c.matched} 
-              onClick={onClick}/>;
-              cardViews.push(cardView);
-        });
-        return cardViews;
-      }
-    
-      clearCards(id1,id2) {
-        if (this.state.numClicksWithinTurn !== 2) {
-          return;
+        super(props)
+        this.state = {
+          frameworks: ['angular2','vue','react','grunt','phantomjs','ember','babel','ionic','gulp','meteor','yeoman','yarn','nodejs','bower','browserify'],
+          duplicatedFrameworks: [],
+          randomizedFrameworks: [],
+          finalizedFrameworks: [],
+          openedFrameworks: []
         }
-        this.memoryCards.flipCard(this.state.firstId, false);
-        this.memoryCards.flipCard(this.state.secondId, false);
-
-        this.setState({
-          firstId: undefined,
-          secondId: undefined,
-          numClicksWithinTurn: 0,
-          turnNo : this.state.turnNo+1
-        });
+        this.start()
       }
-    
-      cardClicked(id,image) {
 
-        if (this.state.numClicksWithinTurn === 0 || this.state.numClicksWithinTurn === 2) {
-          if (this.state.numClicksWithinTurn === 2) {
-            clearTimeout(this.timeout);
-            this.clearCards(this.state.firstId, this.state.secondId);        
+      handleClick(name,index){
+        if(this.state.openedFrameworks.length == 2){
+          setTimeout(() => {
+            this.check()
+          },750)
+        }else {
+          let framework = {
+            name,
+            index
           }
-          this.memoryCards.flipCard(id, true);
+
+          let finalizedFrameworks = this.state.finalizedFrameworks
+          let frameworks = this.state.openedFrameworks
+          finalizedFrameworks[index].close = false
+          frameworks.push(framework)
+
           this.setState({
-            firstId : id,
-            numClicksWithinTurn : 1
-          });
-        } else if (this.state.numClicksWithinTurn === 1) {
-          this.memoryCards.flipCard(id, true);
-          this.setState({
-            secondId : id,
-            numClicksWithinTurn : 2
-          });
-    
-          if (this.memoryCards.cardsHaveIdenticalImages(id, this.state.firstId)) {
-            this.memoryCards.setCardAsMatched(this.state.firstId, true);
-            this.memoryCards.setCardAsMatched(id, true);
-            this.setState({
-              pairsFound: this.state.pairsFound+1,
-              firstId: undefined,
-              secondId: undefined,
-              turnNo : this.state.turnNo+1,
-              numClicksWithinTurn: 0
-            });
-    
-          } else {
-            this.timeout = setTimeout(() => { 
-              this.clearCards(this.state.firstId, this.state.secondId);
-            },5000); 
+            openedFrameworks: frameworks,
+            finalizedFrameworks: finalizedFrameworks
+          })
+
+          if(this.state.openedFrameworks.length == 2){
+            setTimeout(() => {
+              this.check()
+            },750)
           }
         }
-      }
-    
-      playAgain() {
-        this.initGame();
-      }
+      } 
 
-    render() {
-            let cardViews = this.getCardViews();
-        let gameStatus = <div className='Game-status'>
-                        <div>Turn: {this.state.turnNo}</div>
-                        <div>Pairs found: {this.state.pairsFound}</div>
-                        </div>;
+      check(){
+        let finalizedFrameworks = this.state.finalizedFrameworks
 
-        if (this.state.pairsFound === this.memoryCards.NUM_IMAGES) {
-        gameStatus = <div className='Game-status'>
-                        <div>GAME COMPLETE!</div>
-                        <div>You used {this.state.turnNo-1} turns</div>
-                        <div><button onClick={this.onPlayAgain}>Play again?</button></div></div>;      
+        if((this.state.openedFrameworks[0].name == this.state.openedFrameworks[1].name) && (this.state.openedFrameworks[0].index != this.state.openedFrameworks[1].index)){
+          finalizedFrameworks[this.state.openedFrameworks[0].index].complete = true
+          finalizedFrameworks[this.state.openedFrameworks[1].index].complete = true
+        }else {
+          finalizedFrameworks[this.state.openedFrameworks[0].index].close = true
+          finalizedFrameworks[this.state.openedFrameworks[1].index].close = true
         }
 
+        this.setState({
+          finalizedFrameworks,
+          openedFrameworks: []
+        })
+      }
+
+      start(){
+        let finalizedFrameworks = [];
+        this.state.duplicatedFrameworks = this.state.frameworks.concat(this.state.frameworks)
+        this.state.randomizedFrameworks = this.shuffle(this.state.duplicatedFrameworks)
+        this.state.randomizedFrameworks.map((name,index) => {
+          finalizedFrameworks.push({
+            name,
+            close: true,
+            complete: false,
+            fail: false
+          })
+        })
+        this.state.finalizedFrameworks = finalizedFrameworks
+      }
+
+      shuffle(array){
+        let currentIndex = array.length, temporaryValue, randomIndex;
+
+        while (0 !== currentIndex) {
+          randomIndex = Math.floor(Math.random() * currentIndex);
+          currentIndex -= 1;
+          temporaryValue = array[currentIndex];
+          array[currentIndex] = array[randomIndex];
+          array[randomIndex] = temporaryValue;
+        }
+        return array
+      }
+      render(){
+      
         return (
-         <div className='Game'>
-            <header className='Game-header'>
-              <div className='Game-title'>React Memory Game</div>
-            </header>
-            <div>
-            {gameStatus}
-            </div>
-            <div className='CardContainer'>
-            {cardViews}
-            </div>
-        </div>
+          <div className="playground">
+              {
+                this.state.finalizedFrameworks.map((framework, index) => {
+                  return <Card framework={framework.name} click={() => {this.handleClick(framework.name,index)}} close={framework.close} complete={framework.complete}/>
+                })
+              }
+          </div>
         )
-    }
+      }
 }
